9e3c59ead9efcb2a52e02beef46e329f37a1a2d3
Dion Barrick <dion.barrick@jhuapl.edu>
2022-10-03 20:33:02 -0400

    update emacs customization files

--- custom-file.el
+++ custom-file.el
@@ -3,11 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-completion-style (quote helm))
- '(org-roam-directory "/Users/dion/emacs/org-roam/" t)
  '(package-selected-packages
-   (quote
-    (org-roam org-appear mixed-pitch orgalist org-contrib company-jedi impatient-mode git-messenger magit org-roam-bibtex ivy-bibtex helm-bibtex org-ref company-posframe org-superstar which-key helm org-plus-contrib org ox-pandoc ox-gfm vhdl-tools markdown-changelog markdown-preview-mode markdown-toc edit-indirect grip-mode org-mode markdown-mode spacemacs-theme neotree highlight-indent-guides company use-package))))
+   '(counsel-projectile projectile hydra evil-collection evil general all-the-icons helpful counsel ivy-rich which-key rainbow-delimiters ivy spacemacs-common command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
