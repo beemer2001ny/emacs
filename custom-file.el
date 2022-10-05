dc3e40df024c44797866ff7121dcde41a9971e9e
Dion Barrick <dion.barrick@jhuapl.edu>
2022-10-05 11:07:48 -0400

    directly copy over system crafters init.el with comments to use copied from https://github.com/daviwil/emacs-from-scratch/blob/master/init.el

--- custom-file.el
+++ custom-file.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(counsel-projectile projectile hydra evil-collection evil general all-the-icons helpful counsel ivy-rich which-key rainbow-delimiters ivy spacemacs-common command-log-mode use-package)))
+   '(visual-fill org-bullets org counsel-projectile projectile hydra evil-collection evil general all-the-icons helpful counsel ivy-rich which-key rainbow-delimiters ivy spacemacs-common command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
