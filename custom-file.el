c84cb49e33de98b85e5e7e023401af98be6098ef
Dion Barrick <dion.barrick@gmail.com>
2022-11-11 12:06:33 -0500

    updates to emacs. todo: add projectile and magit

--- custom-file.el
+++ custom-file.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(visual-fill org-bullets org counsel-projectile projectile hydra evil-collection evil general all-the-icons helpful counsel ivy-rich which-key rainbow-delimiters ivy spacemacs-common command-log-mode use-package)))
+   '(cua-mode counsel-projectile projectile hydra general spacemacs-theme doom-modeline helpful which-key rainbow-delimiters use-package ivy-rich ivy-prescient counsel command-log-mode auto-package-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
